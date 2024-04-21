962525a9a2c6aa3d513e96d85545f9032ab3c579
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2024-04-21 15:06:54 +1000

    Timer now times

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Simple timer extension for Chrome.
 - Option for favourite times? 
 - Add a pause timer button
 - Add a reset timer button
+- Fix bug which incorrectly converts time (e.g. "1h30m0s" is displayed as "1h29m60s")
--- timer.js
+++ timer.js
@@ -3,14 +3,9 @@ let started = false;
 let beginTime;
 let timerLength;
 let finishTime;
+let secondsPassed = 0;
 
 function initTimer() {
-    // const startTime = new Date();
-    // console.log(startTime);
-    // console.log(startTime.getTime());
-    // let finishTimeOffset = 3600000;
-    // const finishTime = new Date(startTime.getTime() + finishTimeOffset);
-    
     document.getElementById("timerToggle").addEventListener("click", function() {
         if (started) {
             stopTimer();
@@ -21,7 +16,7 @@ function initTimer() {
             seconds = document.getElementById("seconds").value * 1;
             minutes = document.getElementById("minutes").value * 1;
             hours = document.getElementById("hours").value * 1;
-            console.log(hours + "h" + minutes + "m" + seconds + "s");
+            document.getElementById("timeRemaining").innerText = formatTime(hours, minutes, seconds);
             timerLength = (hours * 3600 + minutes * 60 + seconds) * 1000;
             finishTime = new Date(beginTime.getTime() + timerLength);
             document.getElementById("timeStarted").innerText = beginTime.toLocaleTimeString();
@@ -35,12 +30,22 @@ function initTimer() {
 
 function startTimer() {
     timerInterval = setInterval(function() {
-        console.log(new Date().toLocaleTimeString());
+        // Account for inaccuracy in timer interval
+        secondsPassed++;
+        timerLength -= 1000 + (Date.now()-beginTime.getTime()) - 1000 * secondsPassed;
+        hours = Math.floor(timerLength / 3600000);
+        minutes = Math.floor((timerLength / 60000) % 60);
+        seconds = Math.round((timerLength / 1000) % 60);
+        document.getElementById("timeRemaining").innerText = formatTime(hours, minutes, seconds);
     }, 1000)
 }
 
 function stopTimer() {
     clearInterval(timerInterval);
+}
+
+function formatTime(hours, minutes, seconds) {
+    return hours.toString() + "h, " + minutes.toString() + "m, " + seconds.toString() + "s";
 }
 
 addEventListener("DOMContentLoaded", initTimer);
