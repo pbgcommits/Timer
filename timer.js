58179155708d95924f5dc44929f5ed4447ca80c0
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2024-04-21 14:09:53 +1000

    Added finish time calculation

--- timer.js
+++ timer.js
@@ -1,11 +1,47 @@
-function timer() {
-    const startTime = new Date();
-    console.log(startTime);
-    console.log(startTime.getTime());
-    document.getElementById("timeStarted").innerText = startTime.toLocaleTimeString();
-    let finishTimeOffset = 3600000;
-    const finishTime = new Date(startTime.getTime() + finishTimeOffset);
-    document.getElementById("finishTime").innerText = finishTime.toLocaleTimeString();
+let timerInterval;
+let started = false;
+let beginTime;
+let timerLength;
+let finishTime;
+
+function initTimer() {
+    // const startTime = new Date();
+    // console.log(startTime);
+    // console.log(startTime.getTime());
+    // let finishTimeOffset = 3600000;
+    // const finishTime = new Date(startTime.getTime() + finishTimeOffset);
+    
+    document.getElementById("timerToggle").addEventListener("click", function() {
+        if (started) {
+            stopTimer();
+            document.getElementById("timerToggle").innerText = "Start timer";
+        }
+        else {
+            beginTime = new Date();
+            seconds = document.getElementById("seconds").value * 1;
+            minutes = document.getElementById("minutes").value * 1;
+            hours = document.getElementById("hours").value * 1;
+            console.log(hours + "h" + minutes + "m" + seconds + "s");
+            timerLength = (hours * 3600 + minutes * 60 + seconds) * 1000;
+            finishTime = new Date(beginTime.getTime() + timerLength);
+            document.getElementById("timeStarted").innerText = beginTime.toLocaleTimeString();
+            document.getElementById("finishTime").innerText = finishTime.toLocaleTimeString();
+            document.getElementById("timerToggle").innerText = "Stop timer";
+            startTimer();
+        }
+        started = !started;
+    })
 }
 
-addEventListener("DOMContentLoaded", timer);
+function startTimer() {
+    timerInterval = setInterval(function() {
+        console.log(new Date().toLocaleTimeString());
+    }, 1000)
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+}
+
+addEventListener("DOMContentLoaded", initTimer);
+
